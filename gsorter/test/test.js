b5f5bcde8fe0f7d2b0b14527abbc8b75892b8a04
Ali Muhammed <uniapi@outlook.com>
2018-09-14 21:47:32 +0300

    Added [test/test.js] - the file to test gSorter

---
+++ gsorter/test/test.js
@@ -0,0 +1,12 @@
+/*************************************************************************
+                          Written by Ali Muhammed
+                            uniapi@outlook.com
+                            September 14, 2018
+*************************************************************************/
+
+import * as assert from 'assert'
+import { gSorter } from '../dist/index.js'
+
+describe('gSorter', () => {
+  it('1', () => assert.strictEqual(true, true));
+});
